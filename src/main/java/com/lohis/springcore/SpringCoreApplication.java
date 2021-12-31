a1a356e2591fd3e318fec9db77fb9fec893a5735
lohism <abhimv311@gmail.com>
2021-12-31 14:07:36 +0000

    second commit

--- src/main/java/com/lohis/springcore/SpringCoreApplication.java
+++ src/main/java/com/lohis/springcore/SpringCoreApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+
+		SpringApplication app = new SpringApplication(SpringCoreApplication.class);
+		app.run(args);
 	}
 
 }
